9abc90e2c44c9852315e33eddc4bf695c37b808a
BastaBikerSW33TLOVER <francejosephcabral@gmail.com>
2025-07-08 11:36:24 +0800

    need ay save naman

---
+++ lib/world_time/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/choose_location.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      routes: {
+        '/': (context) => const Loading(),
+        '/home': (context) => const Home(),
+        '/location': (context) => const ChooseLocation(),
+      },
+    ),
+  );
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hahhhahaha/main.dart';
+import 'package:hahhhahaha/world_time/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
